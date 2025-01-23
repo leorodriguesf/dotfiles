5288df8142b81337afd23afb04bff9643c75f6b2
Leonardo Rodrigues Ferreira <leo.rf.developer@gmail.com>
2025-01-22 21:05:56 -0300

    Add scripts that boost productivity inside terminal

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,2 +1,26 @@
 # Use vim style line editing in zsh
 bindkey -v
+
+tmux-sessionizer-widget() {
+    exec </dev/tty
+    exec <&1
+    tmux-sessionizer
+    zle redisplay
+}
+
+zle -N tmux-sessionizer-widget
+bindkey -M emacs '^F' tmux-sessionizer-widget
+bindkey -M vicmd '^F' tmux-sessionizer-widget
+bindkey -M viins '^F' tmux-sessionizer-widget
+
+tmux-query-widget() {
+    exec </dev/tty
+    exec <&1
+    tmux-query
+    zle redisplay
+}
+
+zle -N tmux-query-widget
+bindkey -M emacs '^G' tmux-query-widget
+bindkey -M vicmd '^G' tmux-query-widget
+bindkey -M viins '^G' tmux-query-widget
