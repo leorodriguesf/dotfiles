cfd8973177935360d3a4e294b03acebfb9afb9ed
Leonardo Rodrigues Ferreira <leo.rf.developer@gmail.com>
2025-01-24 18:11:05 -0300

    Make sure to always be inside a tmux session to enhance productivity using custom scripts

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,4 +5,6 @@ alias mkdir="mkdir -p"
 alias mv="mv -i"
 alias cp="cp -i"
 
+# If inside tmux session, onle opens nvim, otherwise, open a new tmux-session with nvim
+alias nvim="([ -z "$TMUX" ] && tmux new-session nvim) ||  nvim"
 alias vim="nvim"
